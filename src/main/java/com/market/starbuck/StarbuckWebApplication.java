554c1092f9ec916e3a2d91acd4380015c60603d5
SOLokill <shuai1593@gmail.com>
2022-03-13 20:35:42 +0900

    first commit

---
+++ src/main/java/com/market/starbuck/StarbuckWebApplication.java
@@ -0,0 +1,13 @@
+package com.market.starbuck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbuckWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarbuckWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/market/starbuck/StarbuckWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.starbuck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbuckWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
